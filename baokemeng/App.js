c0e4c7c088b64ae21d8047a63b3b6cfa25bd1fc2
ecko4869 <34934566+ecko4869@users.noreply.github.com>
2018-05-29 16:38:47 +0800

    11

--- baokemeng/App.js
+++ baokemeng/App.js
@@ -4,55 +4,114 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+
+import React, {Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    ScrollView,
+    Image,
+    Button,
+    TouchableHighlight,
+    Modal,
+    TextInput,
+    Dimensions,
+    Alert
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const width = Dimensions.get.width
+const height= Dimensions.get.height
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            number:"",
+            name_chs:"",
+            name_jp:"",
+            name_eng:"",
+            trait1:"",
+            trait2:"",
+            trait3:"",
+            hp:"",
+            atk:"",
+            def:"",
+            satk:"",
+            sdef:"",
+            spd:"",
+            sum:"",
+            type1:"",
+            type2:"",
+            weight:"",
+            sex:"",
+            capture:"",
+            egggroup1:"",
+            egggroup2:"",
+            lay:"",
+            exp:"",
+        };
+    }
+ /*   _keyExtractor = (item,index) => index.toString();*/
+add()
+{
+    return 1;
+}
+calculator()
+{
+    return 1;
+}
+    render() {
+        return (
+            <View style={styles.container}>
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+
+
+
+               <View style={styles.row}>
+                   <Button onPress={()=>{this.add()}} title='图鉴' />
+                    <Button onPress={()=>{this.calculator()}} title='计算器' />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'skyblue',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+
+    row: {
+
+        flexDirection: 'row',
+        borderColor: '#f00',
+        borderWidth: 1
+    },
+    cell: {
+        flex: 1,
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#f00'
+    },
+    ceildark:{
+        flex:1,
+        height:50,
+        borderWidth:1,
+        borderColor:'black',
+    }
 });
